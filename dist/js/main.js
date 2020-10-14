373eeea4b895db4dd44db23187f368775bdab390
Mesbah <msbh214@gmail.com>
2020-10-14 15:59:28 +0600

    initial commit

--- dist/js/main.js
+++
@@ -1,1 +0,0 @@
-app.global={init:function(){console.log("load global functions"),app.global.loadHeader()},loadHeader:function(){console.log("loadHeader()")}},app.global.init(),console.log("Load script.js");var app={};
